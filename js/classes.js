e29d9215570e8ee049fd9e02ff96e062416695c2
unknown <jorgen.sater@gmail.com>
2020-01-30 17:41:33 +1100

    Moved decleration of all classes to classes.js

---
+++ js/classes.js
@@ -0,0 +1,45 @@
+class CraftingRequirement{
+    constructor(craftingItem, quantity){
+        this.craftingItem = craftingItem;
+        this.quantity = quantity;
+    }
+
+    toString(){
+        this.craftingItem.name + ' : ' + this.quantity;
+    }
+}
+
+class CraftingItem{
+    constructor(name, craftingTime, cost, craftingRequirements){
+        this.name = name;
+        this.craftingTime = craftingTime;
+        this.cost = cost;
+
+        this.craftingRequirements = craftingRequirements;
+    }
+
+    toString(){
+        var craftReq = "";
+        for(var i = 0; i < this.craftingRequirements.length; i++){
+            craftReq += this.craftingRequirements[i].craftingItem.name + ' : ' + this.craftingRequirements[i].quantity + " ";
+        }
+        return this.name + ", " + this.craftingTime + "minutes, " + this.cost + "coins. Crafting Requirement: " + craftReq;
+    }
+    
+    getCost(time, quantity){
+        const halfPrice = Math.ceil(this.cost / 2);
+        var crafts = 1;
+        if(this.craftingTime < time){
+            crafts = Math.ceil(time / this.craftingTime);
+        }
+
+        if(crafts > quantity){
+            crafts = quantity;
+        }
+        var itemsPerCraft = Math.ceil(quantity / crafts);
+
+        var cost = (this.cost / 4) * (Math.pow(itemsPerCraft, 2) + 3 * itemsPerCraft) * crafts;
+
+        return [cost, itemsPerCraft, crafts];
+    }
+}
