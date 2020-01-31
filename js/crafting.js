d733020c802b698e20d01eb14156147c6e0c984e
unknown <jorgen.sater@gmail.com>
2020-02-01 03:24:05 +1100

    fixed crafting items with wrong rarity

--- js/crafting.js
+++ js/crafting.js
@@ -14,11 +14,11 @@ string = new CraftingItem("String", 5, 50, rarity.COMMON, stringReq);
 craftingRecipes.set(string.name, string);
 
 var woodReq = [new CraftingRequirement(log, 3)];
-wood = new CraftingItem("Wood", 5, 50, rarity.RARE, woodReq);
+wood = new CraftingItem("Wood", 5, 50, rarity.COMMON, woodReq);
 craftingRecipes.set(wood.name, wood);
 
 var ribbonReq = [new CraftingRequirement(string, 2), new CraftingRequirement(wood, 2)];
-ribbon = new CraftingItem("Ribbon", 15, 100, rarity.EPIC, ribbonReq);
+ribbon = new CraftingItem("Ribbon", 15, 100, rarity.RARE, ribbonReq);
 craftingRecipes.set(ribbon.name, ribbon);
 
 // Shiny Crafting
@@ -75,11 +75,11 @@ craftingRecipes.set(fire.name, fire);
 
 // Ancient Crafting
 var waterstoneReq = [new CraftingRequirement(water, 2), new CraftingRequirement(ribbon, 10)];
-waterstone = new CraftingItem("Waterstone", 720, 1500, rarity.COMMON, waterstoneReq);
+waterstone = new CraftingItem("Waterstone", 720, 1500, rarity.RARE, waterstoneReq);
 craftingRecipes.set(waterstone.name, waterstone);
 
 var firestoneReq = [new CraftingRequirement(fire, 1), new CraftingRequirement(sparkles, 2)];
-firestone = new CraftingItem("Firestone", 720, 1500, rarity.RARE, firestoneReq);
+firestone = new CraftingItem("Firestone", 720, 1500, rarity.EPIC, firestoneReq);
 craftingRecipes.set(firestone.name, firestone);
 
 var elementstoneReq = [new CraftingRequirement(firestone, 1), new CraftingRequirement(waterstone, 1)];
