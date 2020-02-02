d673983dc1878df6704de6c01b6a0dbfa0c44913
unknown <jorgen.sater@gmail.com>
2020-02-03 04:41:44 +1100

    Changed calculate cooldown to 1second

--- js/globals.js
+++ js/globals.js
@@ -2,7 +2,7 @@ var craftingRecipes = new Map(); // Used to store all the different crafting rec
 const outputRows = 4; // How many crafting items per row in the output
 var currentCraft = new Map(); // Stores the current items the user have selected to calculate
 var lastTimeCalculated = 100; // Stores last time the "calculate" button was pressed
-const calculateDelay = 3 * 1000; // Stores how many ms delay it should be between the user is allowed to click "calculate"
+const calculateDelay = 1 * 1000; // Stores how many ms delay it should be between the user is allowed to click "calculate"
 
 const rarity = {
     HIDDEN: 'hidden',
