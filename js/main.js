8db02c3c260855645ca8c4c6689beb62c5e8fb35
unknown <jorgen.sater@gmail.com>
2020-02-13 17:57:24 +1100

    Removed code regarding cookie from main.js

--- js/main.js
+++ js/main.js
@@ -4,45 +4,6 @@ function init(){
     document.getElementById("calculate").onclick = calculate;
     document.getElementById("clear").onclick = clear;
     document.getElementById("copyClipboard").onclick = copyClipboard;
-    
-    document.getElementById("cookieAccept").onclick = cookieAccept;
-    checkCookie();
-}
-
-/**
- * Check if the user have already accepted the cookie policy
-*/
-function checkCookie(){
-    var cookie = getCookie(cookieName);
-    if(cookie !== cookieValue){
-        var cookieBanner = document.getElementById("cookiePolicyBanner");
-        cookieBanner.style.display = "flex";
-    }
-}
-
-/*
-    User accepted our cookie policy
-*/
-function cookieAccept(){
-    setCookie(cookieName, cookieValue, 365);
-    var cookieBanner = document.getElementById("cookiePolicyBanner");
-    cookieBanner.style.display = "none";
-}
-
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-
-function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
 /*
