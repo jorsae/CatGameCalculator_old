d480117f41ba92eb466f503936d1e7788b617386
unknown <jorgen.sater@gmail.com>
2020-02-21 07:02:21 +1100

    added floor 10

--- src/app/floors.js
+++ src/app/floors.js
@@ -7,8 +7,7 @@ floorRecipes.set(basic.name, basic);
 
 /**
  * Floors added:
- * 7
- * 8
+ * 7, 8, 9, 10
  */
 
 const plantReq = [new CraftingRequirement(craftingRecipes.get("Ribbon"), 6),
@@ -30,3 +29,23 @@ const forestReq = [new CraftingRequirement(craftingRecipes.get("Wood"), 7),
                 ];
 const forest = new Floor("Forest", 8, forestReq);
 floorRecipes.set(forest.name, forest);
+
+const dessertReq = [new CraftingRequirement(craftingRecipes.get("Ribbon"), 14),
+                    new CraftingRequirement(craftingRecipes.get("Metal"), 20),
+                    new CraftingRequirement(craftingRecipes.get("Needles"), 7),
+                    new CraftingRequirement(craftingRecipes.get("Sparkles"), 4),
+                ];
+const dessert = new Floor("Dessert", 9, dessertReq);
+floorRecipes.set(dessert.name, dessert);
+
+const dreamReq = [new CraftingRequirement(craftingRecipes.get("Wood"), 2),
+                    new CraftingRequirement(craftingRecipes.get("Needles"), 11),
+                    new CraftingRequirement(craftingRecipes.get("Sparkles"), 5),
+                    new CraftingRequirement(craftingRecipes.get("Metal"), 3),
+                    new CraftingRequirement(craftingRecipes.get("Ribbon"), 6),
+                    new CraftingRequirement(craftingRecipes.get("Bronze"), 8),
+                    new CraftingRequirement(craftingRecipes.get("Silver"), 1),
+                ];
+const dream = new Floor("Dream", 10, dreamReq);
+floorRecipes.set(dream.name, dream);
+
